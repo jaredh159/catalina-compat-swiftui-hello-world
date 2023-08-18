3600f1dba2a2a1dede0fbf242f81994398c670df
Jared Henderson <jaredh159@users.noreply.github.com>
2023-08-18 10:45:23 -0400

    initial commit

---
+++ SwiftUIHelloWorld/main.swift
@@ -0,0 +1,29 @@
+import AppKit
+import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  var window: NSWindow!
+
+  func applicationDidFinishLaunching(_: Notification) {
+    let contentView = ContentView()
+      .frame(minWidth: 300, minHeight: 200)
+      .edgesIgnoringSafeArea(.all)
+
+    window = NSWindow(
+      contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+      styleMask: [.titled, .closable, .miniaturizable, .resizable],
+      backing: .buffered,
+      defer: false
+    )
+
+    window.center()
+    window.title = "Swift UI Hello World"
+    window.contentView = NSHostingView(rootView: contentView)
+    NSApp.activate(ignoringOtherApps: true)
+    window.makeKeyAndOrderFront(nil)
+  }
+}
+
+let appDelegate = AppDelegate()
+NSApplication.shared.delegate = appDelegate
+NSApplication.shared.run()
