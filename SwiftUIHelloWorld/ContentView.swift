988140f84b3f4b4ea41eb54a0f627827c132171a
Jared Henderson <jaredh159@users.noreply.github.com>
2023-08-18 12:33:29 -0400

    add codeblock that triggers catalina crash

--- SwiftUIHelloWorld/ContentView.swift
+++ SwiftUIHelloWorld/ContentView.swift
@@ -1,9 +1,18 @@
-
 import SwiftUI
 
 struct ContentView: View {
   var body: some View {
     Text("Hello, Catalina!").padding()
+  }
+}
+
+public extension View {
+  @available(iOS 14, tvOS 14, watchOS 7, *)
+  @available(macOS, unavailable)
+  func testAvailableCrash() -> some View {
+    self.fullScreenCover(isPresented: .constant(true)) {
+      Text("Unreachable!")
+    }
   }
 }
 
